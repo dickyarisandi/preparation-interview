29e9d41ac52b39065fa5aa7b301848db20511da7
Dicky <dickyarisandihm4@gmail.com>
2022-06-09 08:58:55 +0700

    preparation interview

--- find_middle_alphabet/main.go
+++ find_middle_alphabet/main.go
@@ -30,7 +30,6 @@ func middleAlphabet(alphabets, firstAlphabet, lastAlphabet string) string {
 	firstAlphabet = strings.ToUpper(firstAlphabet)
 	lastAlphabet = strings.ToUpper(lastAlphabet)
 
-	//! without looping
 	first := float64(strings.Index(alphabets, firstAlphabet))
 	last := float64(strings.Index(alphabets, lastAlphabet))
 
